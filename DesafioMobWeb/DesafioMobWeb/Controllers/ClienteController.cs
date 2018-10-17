2215055a1204b568292edcaedb72e3b3ec86c9b1
Robson Paiva <paivawork@hotmail.com>
2018-10-17 11:50:51 -0300

    Modificação do Rooute e ajustes de ClienteController

--- DesafioMobWeb/DesafioMobWeb/Controllers/ClienteController.cs
+++ DesafioMobWeb/DesafioMobWeb/Controllers/ClienteController.cs
@@ -1,50 +1,57 @@
-﻿using DesafioMobWeb.Models;
+﻿using DesafioMobWeb.Context;
+using DesafioMobWeb.Models;
 using System.Web.Mvc;
 
 namespace DesafioMobWeb.Controllers
 {
     public class ClienteController : Controller
     {
+        public ContextoDB db = new ContextoDB();
+
+        var insercaoDeDadosCliente = new Cliente
+        {
+            ClienteID = 1,
+            RazaoSocialCliente = "Robson Paiva Consultoria",
+            FantasiaCliente = "Consultoria SystemsPaiva",
+            CnpjCliente = "99.999.999/0001-01",
+            RuaCliente = "Rua",
+            NumeroRuaCliente = 99,
+            ComplementoCliente = "Sem complemento",
+            BairroCliente = "Bairro",
+            MunicipioCliente = "Cidade",
+            CepCliente = "60000-000"
+        };
+
         public ActionResult Cliente()
         {
-            var cliente = new Cliente
-            {
-                ClienteID = 1,
-                RazaoSocialCliente = "Robson Paiva Consultoria",
-                FantasiaCliente = "Consultoria SystemsPaiva",
-                CnpjCliente = "99.999.999/0001-01",
-                RuaCliente = "Rua",
-                NumeroRuaCliente = 99,
-                ComplementoCliente = "Sem complemento",
-                BairroCliente = "Bairro",
-                MunicipioCliente = "Cidade",
-                CepCliente = "60000-000"
-            };
+            
 
-            return View(cliente);
+            return View(insercaoDeDadosCliente);
         }
 
 
         public ActionResult Cadastrar()
         {
-            var dadosCliente = new Cliente
-            {
-                ClienteID = 1,
-                RazaoSocialCliente = "Robson Paiva Consultoria",
-                FantasiaCliente = "Consultoria SystemsPaiva",
-                CnpjCliente = "99.999.999/0001-01",
-                RuaCliente = "Rua",
-                NumeroRuaCliente = 99,
-                ComplementoCliente = "Sem complemento",
-                BairroCliente = "Bairro",
-                MunicipioCliente = "Cidade",
-                CepCliente = "60000-000"
-            };
-
-            return View(dadosCliente);
+            return View(insercaoDeDadosCliente);
         }
 
-        public ActionResult Detalhes(int id)
+        [HttpPost]
+        public ActionResult Cadastrar(Cliente cli)
+        {
+            try
+            {
+                db.Clientes.Add(cli);
+                db.SaveChanges();
+
+                return RedirectToAction("Editar");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Visualizar(int id)
         {
             return View();
         }
