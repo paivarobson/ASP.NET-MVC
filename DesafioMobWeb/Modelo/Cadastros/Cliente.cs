49aee4a2f176a589b9f2290aff065bbabfc0bab1
Robson Paiva <paivawork@hotmail.com>
2018-10-18 14:41:53 -0300

    DataAnnotations Display nas Models Usuario e Cliente

--- DesafioMobWeb/Modelo/Cadastros/Cliente.cs
+++ DesafioMobWeb/Modelo/Cadastros/Cliente.cs
@@ -1,25 +1,37 @@
-﻿namespace Modelo.Cadastros
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Modelo.Cadastros
 {
     public class Cliente
     {
+        [Display(Name = "Código")]
         public int ClienteID { get; set; }
 
+        [Display(Name = "Razão Social")]
         public string RazaoSocialCliente { get; set; }
 
+        [Display(Name = "Fantasia")]
         public string FantasiaCliente { get; set; }
 
+        [Display(Name = "CNPJ")]
         public string CnpjCliente { get; set; }
 
+        [Display(Name = "Logradouro")]
         public string RuaCliente { get; set; }
 
+        [Display(Name = "Número")]
         public int NumeroRuaCliente { get; set; }
 
+        [Display(Name = "Bairro")]
         public string BairroCliente { get; set; }
 
+        [Display(Name = "Complemento")]
         public string ComplementoCliente { get; set; }
 
+        [Display(Name = "Município")]
         public string MunicipioCliente { get; set; }
 
+        [Display(Name = "CEP")]
         public string CepCliente { get; set; }
 
     }
--- DesafioMobWeb/Modelo/Cadastros/Usuario.cs
+++ DesafioMobWeb/Modelo/Cadastros/Usuario.cs
@@ -1,19 +1,29 @@
-﻿namespace Modelo.Cadastros
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Modelo.Cadastros
+
 {
     public class Usuario
     {
+        [Display(Name = "Código")]
         public int UsuarioID { get; set; }
 
+        [Display(Name = "Nome")]
         public string NomeUsuario { get; set; }
 
+        [Display(Name = "Apelido")]
         public string ApelidoUsuario { get; set; }
 
+        [Display(Name = "Senha")]
         public string SenhaUsuario { get; set; }
 
+        [Display(Name = "Confirmar Senha")]
         public string ConfirmarSenhaUsuario { get; set; }
 
+        [Display(Name = "E-mail")]
         public string EmailUsuario { get; set; }
 
+        [Display(Name = "Telefone")]
         public string TelefoneUsuario { get; set; }
     }
 }
